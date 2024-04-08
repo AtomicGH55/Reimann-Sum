555c6989cfce25dbe340bef472677728e90a60bc
AtomicGH55 <bradycalhoun@gmail.com>
2024-04-08 10:53:26 -0400

    a

--- reimann_main.py
+++ reimann_main.py
@@ -1,33 +1,37 @@
 import matplotlib.pyplot as plt
 
-
-
-
-
 def main():
 
     fig, ax = plt.subplots()
-    counter = -20
+
+    seg_len = 1
+    counter = -10
     x_points = []
     y_points = []
 
-    #reimann_style = input('Type "L" for left, "R" for right, "M" for middle, and "T" for trapazoid: ')
+    
 
-    while counter < 21:
-        
-        current_output = counter^2
+    while counter <= 10:
+
+        polynomial = counter**2 + 3*counter + 4
 
         x_points.append(counter)
-        y_points.append(current_output)
-        
+        y_points.append(polynomial)
+
         counter = counter + 1
-    print(x_points)
-    print(y_points)
-        
-    ax.plot(x_points, y_points)
+
+        ax.plot(x_points, y_points, "ro")
+
+
+    #left_area = left_sum(y_points, seg_len)
+    print(len(y_points))
     plt.show()
-        
 
 if __name__ == '__main__':
     main()
-   
+
+def left_sum(y_points, seg_len):
+    for i in y_points:
+        left_area = y_points*seg_len
+
+    left_area = left_area - (y_points[len(y_points)-1])
