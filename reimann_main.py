6f8697e625465bf441f6d369afcc29a437c97ca9
AtomicGH55 <bradycalhoun@gmail.com>
2024-04-18 10:25:32 -0400

    fix midpoint

--- reimann_main.py
+++ reimann_main.py
@@ -1,39 +1,71 @@
 import matplotlib.pyplot as plt
 
+def left_sum(y_points, seg_len):
+    del y_points[len(y_points)-1]
+    left_area = 0
+    for i in range(len(y_points)):
+        current_area = y_points[i]*seg_len
+        print(current_area)
+        left_area = left_area + current_area
+
+    return left_area
+
+def right_sum(y_points, seg_len):
+    del y_points[0]
+    right_area = 0
+    for i in range(len(y_points)):
+        current_area = y_points[i]*seg_len
+        print(current_area)
+        right_area = right_area + current_area
+
+    return right_area
+
+def mid_sum(y_points, seg_len):
+    del y_points[len(y_points)-1]
+    mid_area = 0
+    for i in range(len(y_points)-1):
+        current_area = ((y_points[i]+y_points[i+1])/2)*seg_len
+        print(current_area)
+        mid_area = mid_area + current_area
+
+
+
+    return mid_area
+
 def main():
 
     fig, ax = plt.subplots()
 
-    seg_len = 1
-    counter = -10
+    min_x = -10
+    max_x = 10
+    seg_ammount = 4
+    seg_len = (max_x-min_x)/seg_ammount
+    
     x_points = []
     y_points = []
 
-    
+    counter = min_x
 
-    while counter <= 10:
+    while counter <= max_x:
 
-        polynomial = counter**3 
+        polynomial = counter**3 +3*counter**2 - 10
 
         x_points.append(counter)
         y_points.append(polynomial)
 
-        counter = counter + 1
+        counter = counter + seg_len
         
         ax.plot(x_points, y_points)
         ax.plot(x_points, y_points, "ro")
-        
+    print(y_points)
 
 
-    #left_area = left_sum(y_points, seg_len)
-    print(len(y_points))
+    area = mid_sum(y_points, seg_len)
+
+
+    print("Area =", area)
     plt.show()
 
 if __name__ == '__main__':
     main()
 
-def left_sum(y_points, seg_len):
-    for i in y_points:
-        left_area = y_points*seg_len
-
-    left_area = left_area - (y_points[len(y_points)-1])
