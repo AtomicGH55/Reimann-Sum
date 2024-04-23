d7aed54de3ba131c31354f80c093c5c41a462839
AtomicGH55 <bradycalhoun@gmail.com>
2024-04-23 14:51:51 -0400

    lol

--- reimann_main.py
+++ reimann_main.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
+def polynomial(counter):
+
+    polynomial = counter**3 +3*counter**2 - 10
+
+    return polynomial
+
 def left_sum(y_points, seg_len):
     del y_points[len(y_points)-1]
     left_area = 0
@@ -20,17 +26,29 @@ def right_sum(y_points, seg_len):
 
     return right_area
 
-def mid_sum(y_points, seg_len):
-    del y_points[len(y_points)-1]
+def mid_sum(x_points, seg_len):
     mid_area = 0
-    for i in range(len(y_points)-1):
-        current_area = ((y_points[i]+y_points[i+1])/2)*seg_len
+    for i in range(len(x_points)-1):
+        current_area = (polynomial((x_points[i]+x_points[i+1])/2))*seg_len
         print(current_area)
         mid_area = mid_area + current_area
 
-
-
     return mid_area
+
+def trap_sum(y_points, seg_len):
+    trap_area = 0
+    for i in range(len(y_points)):
+        current_area = y_points[i]*(seg_len/2)
+        print(current_area)
+        trap_area = trap_area + current_area
+    del y_points[len(y_points)-1]
+    del y_points[0]
+    for j in range(len(y_points)):
+        current_area = y_points[j]*(seg_len/2)
+        print(current_area)
+        trap_area = trap_area + current_area
+
+    return trap_area
 
 def main():
 
@@ -38,31 +56,87 @@ def main():
 
     min_x = -10
     max_x = 10
-    seg_ammount = 4
+    x_axis = [min_x, max_x]
+    seg_ammount = 10
     seg_len = (max_x-min_x)/seg_ammount
+    
     
     x_points = []
     y_points = []
+    left_side_x = []
+    left_side_y = []
+    right_side_x = []
+    right_side_y = []
+    top_x = []
+    top_y = []
 
     counter = min_x
 
-    while counter <= max_x:
+    sum_choice = input("Type r for Right, l for Left, m for Mid, t for Trap: ")
 
-        polynomial = counter**3 +3*counter**2 - 10
+    if sum_choice == "r":
 
-        x_points.append(counter)
-        y_points.append(polynomial)
+        while counter <= max_x:
 
-        counter = counter + seg_len
-        
-        ax.plot(x_points, y_points)
-        ax.plot(x_points, y_points, "ro")
-    print(y_points)
+            poly_point = polynomial(counter)
+
+            x_points.append(counter)
+            y_points.append(poly_point)
+
+            counter = counter + seg_len
+            
+            ax.plot(x_points, y_points)
+            ax.plot(x_points, y_points, "ro")
+        print(y_points)
+        area = right_sum(y_points, seg_len)
+
+    elif sum_choice == "l":
+        while counter <= max_x:
+
+            poly_point = polynomial(counter)
+
+            x_points.append(counter)
+            y_points.append(poly_point)
+
+            counter = counter + seg_len
+            
+            ax.plot(x_points, y_points)
+            ax.plot(x_points, y_points, "ro")
+        print(y_points)
+        area = left_sum(y_points, seg_len)
+
+    elif sum_choice == "m":
+        while counter <= max_x:
+
+            poly_point = polynomial(counter)
+
+            x_points.append(counter)
+            y_points.append(poly_point)
+
+            counter = counter + seg_len
+            
+            ax.plot(x_points, y_points)
+            ax.plot(x_points, y_points, "ro")
+        print(y_points)
+        area = mid_sum(x_points, seg_len)
+
+    elif sum_choice == "t":
+        while counter <= max_x:
+
+            poly_point = polynomial(counter)
+
+            x_points.append(counter)
+            y_points.append(poly_point)
+
+            counter = counter + seg_len
+            
+            ax.plot(x_points, y_points)
+            ax.plot(x_points, y_points, "ro")
+        print(y_points)
+        area = trap_sum(y_points, seg_len)
 
 
-    area = mid_sum(y_points, seg_len)
-
-
+    ax.plot(x_axis, [0,0])
     print("Area =", area)
     plt.show()
 
